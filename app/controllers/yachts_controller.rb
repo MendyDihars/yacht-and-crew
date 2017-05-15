af7a574c0ffe0932b390a3a85106e5980a83d596
Denis giraud <giraud.de@gmail.com>
2017-05-15 18:28:00 +0200

    added new and create to YachtsController with routes

--- app/controllers/yachts_controller.rb
+++ app/controllers/yachts_controller.rb
@@ -4,4 +4,18 @@ class YachtsController < ApplicationController
   def index
     @yachts = Yacht.all
   end
+
+  def new
+    @yacht = Yacht.new
+  end
+
+  def create
+    @yacht = Yacht.new(yacht_params)
+    @yacht.save ? redirect_to(@yacht) : render(:new)
+  end
+
+  def yacht_params
+     params.require(:yacht).permit(:name, :crew, :description, :address, :max_capacity)
+  end
+
 end
